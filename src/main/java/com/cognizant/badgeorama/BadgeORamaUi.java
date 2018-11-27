fc852ca560ce716c1b5e5e1f40b8bb729fb160cb
Ray Hedgecock <raymondhedgecock@gmail.com>
2018-11-26 20:02:03 -0700

    Initial commit

---
+++ src/main/java/com/cognizant/badgeorama/BadgeORamaUi.java
@@ -0,0 +1,14 @@
+package com.cognizant.badgeorama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BadgeORamaUi
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(BadgeORamaUi.class, args);
+    }
+}
---
+++ src/test/java/com/cognizant/badgeorama/BadgeORamaUiTest.java
@@ -0,0 +1,16 @@
+package com.cognizant.badgeorama;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class BadgeORamaUiTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
