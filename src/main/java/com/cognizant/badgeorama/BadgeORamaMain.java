290feb1c8387cfd34849a0b20f38afd3224453e4
Ray Hedgecock <raymondhedgecock@gmail.com>
2018-11-26 22:41:25 -0700

    initial controller

--- src/main/java/com/cognizant/badgeorama/BadgeORamaMain.java
+++ src/main/java/com/cognizant/badgeorama/BadgeORamaMain.java
@@ -3,7 +3,9 @@ package com.cognizant.badgeorama;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+/**
+ * https://spring.io/guides/tutorials/react-and-spring-data-rest/
+ */
 @SpringBootApplication
 public class BadgeORamaMain
 {
