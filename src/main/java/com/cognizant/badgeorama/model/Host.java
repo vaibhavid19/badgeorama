bf4c5ddc5f4cf4dc7f6ffb5cbb9807571b876b24
Ray Hedgecock <raymondhedgecock@gmail.com>
2018-11-27 18:25:38 -0700

    Adding Host model

---
+++ src/main/java/com/cognizant/badgeorama/model/Host.java
@@ -0,0 +1,40 @@
+package com.cognizant.badgeorama.model;
+
+public class Host {
+
+    private String phoneNumber;
+    private String name;
+
+    public Host() {
+    }
+
+    public Host(String phoneNumber, String name) {
+        this.phoneNumber = phoneNumber;
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Host{");
+        sb.append("phoneNumber='").append(phoneNumber).append('\'');
+        sb.append(", name='").append(name).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
